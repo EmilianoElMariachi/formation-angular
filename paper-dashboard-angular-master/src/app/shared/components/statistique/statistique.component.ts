f633ae44dafd44e66f38368995ea803e36655f0e
Nicolas Amini-Lamy <pro@naminilamy.fr>
2020-06-04 15:39:55 +0200

    Export composant statistique

---
+++ paper-dashboard-angular-master/src/app/shared/components/statistique/statistique.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Statistique } from 'app/shared/models/Statistique';
+import { Appreciation } from 'app/shared/models/Appreciation';
+
+@Component({
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.scss']
+})
+export class StatistiqueComponent implements OnInit {
+
+  @Input()
+  public stat:Statistique;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  getAppreciationClass(stat: Statistique) {
+    switch (stat.getAppreciation()) {
+      case Appreciation.SUCCESS:
+        return "success"
+      case Appreciation.WARNING:
+        return "warning";
+      case Appreciation.ERROR:
+        return "danger";
+    }
+  }
+}
